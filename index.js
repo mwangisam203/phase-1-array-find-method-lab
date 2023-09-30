fd43b24f2de7d19b12d6cc54146d073212aac872
mwangisam203 <mwangisam203@gmail.com>
2023-09-30 16:13:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    for (let i = 0; i < record.length; i++) {
+      if (record[i].result === "W" && record[i].team === "Denver Broncos") {
+        return record[i].year;
+      }
+    }
+    return undefined;
+  }
